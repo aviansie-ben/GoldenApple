4db4fa753d787052ce55e50240df4758f43e13c1
Daniel Andrus <danieleandrus@gmail.com>
2012-06-05 21:02:53 -0600

    Now stores children areas by their IDs.

--- src/main/java/com/bendude56/goldenapple/area/ParentArea.java
+++ src/main/java/com/bendude56/goldenapple/area/ParentArea.java
@@ -3,28 +3,36 @@ package com.bendude56.goldenapple.area;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.bendude56.goldenapple.GoldenApple;
+
 /**
  * This class is the parent of all classes who support child classes.
  * 
  * @author Deaboy
  */
 public class ParentArea extends Area {
-	private List<ChildArea> children = new ArrayList<ChildArea>();
+	private List<Long> childrenIDs = new ArrayList<Long>();
 	
 	public void addChild(ChildArea child) {
-		if (!children.contains(child)) {
-			children.add(child);
+		if (!childrenIDs.contains(child.getID())) {
+			childrenIDs.add(child.getID());
 			child.setParent(this);
 		}
 	}
 	
 	public void removeChild(ChildArea child) {
-		if (children.contains(child)) {
-			children.remove(child);
+		if (childrenIDs.contains(child.getID())) {
+			childrenIDs.remove(child.getID());
 		}
 	}
 	
 	public List<ChildArea> getChildren() {
+		List<ChildArea> children = new ArrayList<ChildArea>();
+		for(Long childID : childrenIDs) {
+			if (GoldenApple.getInstance().areas.getArea(childID) != null) {
+				children.add((ChildArea) GoldenApple.getInstance().areas.getArea(childID));
+			}
+		}
 		return children;
 	}
 }
