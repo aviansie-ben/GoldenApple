bbb3a26a6154cff72f097116483e428eb804f2eb
bendude56 <bendude56@gmail.com>
2012-08-23 19:05:31 -0600

    Block players from using others' locked blocks

--- src/main/java/com/bendude56/goldenapple/listener/LockListener.java
+++ src/main/java/com/bendude56/goldenapple/listener/LockListener.java
@@ -4,9 +4,15 @@ import java.util.logging.Level;
 
 import org.bukkit.event.Event;
 import org.bukkit.event.EventException;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.EventExecutor;
+import org.bukkit.plugin.RegisteredListener;
+
 import com.bendude56.goldenapple.GoldenApple;
+import com.bendude56.goldenapple.User;
+import com.bendude56.goldenapple.lock.LockedBlock;
 
 public class LockListener implements Listener, EventExecutor {
 
@@ -25,18 +31,32 @@ public class LockListener implements Listener, EventExecutor {
 	}
 
 	private void registerEvents() {
-		
+		PlayerInteractEvent.getHandlerList().register(new RegisteredListener(this, this, EventPriority.NORMAL, GoldenApple.getInstance(), true));
 	}
 
 	private void unregisterEvents() {
-		
+		PlayerInteractEvent.getHandlerList().unregister(this);
 	}
 
 	@Override
 	public void execute(Listener listener, Event event) throws EventException {
-		if (false) {
+		if (event instanceof PlayerInteractEvent) {
+			playerInteract((PlayerInteractEvent) event);
 		} else {
 			GoldenApple.log(Level.WARNING, "Unrecognized event in LockListener: " + event.getClass().getName());
+		}
+	}
+	
+	private void playerInteract(PlayerInteractEvent event) {
+		LockedBlock lock = GoldenApple.getInstance().locks.getLock(event.getClickedBlock().getLocation());
+		User u = User.getUser(event.getPlayer());
+		if (lock == null)
+			return;
+		
+		if (!lock.canUse(u)) {
+			GoldenApple.getInstance().locale.sendMessage(u, "error.lock.noUse", false, GoldenApple.getInstance().permissions.getUser(lock.getOwner()).getName());
+			event.setCancelled(true);
+			return;
 		}
 	}
 }
