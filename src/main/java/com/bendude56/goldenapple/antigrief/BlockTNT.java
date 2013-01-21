2578b7ec1ff23985a48a9dae4d1fb0717cbbccb6
bendude56 <bendude56@gmail.com>
2013-01-20 21:47:23 -0700

    Added TNT blocking to Antigrief module

---
+++ src/main/java/com/bendude56/goldenapple/antigrief/BlockTNT.java
@@ -0,0 +1,101 @@
+package com.bendude56.goldenapple.antigrief;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import org.bukkit.Material;
+
+import com.bendude56.goldenapple.GoldenApple;
+
+import net.minecraft.server.v1_4_R1.Block;
+import net.minecraft.server.v1_4_R1.Entity;
+import net.minecraft.server.v1_4_R1.EntityArrow;
+import net.minecraft.server.v1_4_R1.EntityTNTPrimed;
+import net.minecraft.server.v1_4_R1.StepSound;
+import net.minecraft.server.v1_4_R1.World;
+
+public class BlockTNT extends net.minecraft.server.v1_4_R1.BlockTNT {
+	
+	public static void registerBlock() throws Exception {
+		Block.byId[Material.TNT.getId()] = null;
+		Block tnt = prepClass((Block)BlockTNT.class.getConstructors()[0].newInstance(46, 8));
+		
+		Field f = Block.class.getField("TNT");
+		Field mod = Field.class.getDeclaredField("modifiers");
+		mod.setAccessible(true);
+		mod.setInt(f, mod.getInt(f)  & ~Modifier.FINAL);
+		
+		f.set(null, tnt);
+	}
+	
+	public static void unregisterBlock() throws Exception {
+		Block.byId[Material.TNT.getId()] = null;
+		Block tnt = prepClass((Block)net.minecraft.server.v1_4_R1.BlockTNT.class.getConstructors()[0].newInstance(46, 8));
+		
+		Field f = Block.class.getField("TNT");
+		Field mod = Field.class.getDeclaredField("modifiers");
+		mod.setAccessible(true);
+		mod.setInt(f, mod.getInt(f)  & ~Modifier.FINAL);
+		
+		f.set(null, tnt);
+	}
+	
+	private static Block prepClass(Block b) throws Exception {
+		Method m = Block.class.getDeclaredMethod("c", new Class<?>[] { float.class });
+		m.setAccessible(true);
+		m.invoke(b, 0.0F);
+		
+		m = Block.class.getDeclaredMethod("a", new Class<?>[] { StepSound.class });
+		m.setAccessible(true);
+		m.invoke(b, Block.g);
+		
+		b.b("tnt");
+		
+		return b;
+	}
+
+	public BlockTNT(int i, int j) {
+		super(i, j);
+	}
+	
+	@Override
+	public void onPlace(World world, int i, int j, int k) {
+        super.onPlace(world, i, j, k);
+        if (!world.suppressPhysics && world.isBlockIndirectlyPowered(i, j, k) && !GoldenApple.getInstance().mainConfig.getBoolean("modules.antigrief.noRedstoneTnt", true)) {
+            this.postBreak(world, i, j, k, 1);
+            world.setTypeId(i, j, k, 0);
+        }
+    }
+	
+	@Override
+	public void doPhysics(World world, int i, int j, int k, int l) {
+        if (l > 0 && Block.byId[l].isPowerSource() && world.isBlockIndirectlyPowered(i, j, k) && !GoldenApple.getInstance().mainConfig.getBoolean("modules.antigrief.noRedstoneTnt", true)) {
+            this.postBreak(world, i, j, k, 1);
+            world.setTypeId(i, j, k, 0);
+        }
+    }
+	
+	@Override
+	public void wasExploded(World world, int i, int j, int k) {
+        if (!world.isStatic  && !GoldenApple.getInstance().mainConfig.getBoolean("modules.antigrief.noExplosionTnt", true)) {
+            EntityTNTPrimed entitytntprimed = new EntityTNTPrimed(world, (double) ((float) i + 0.5F), (double) ((float) j + 0.5F), (double) ((float) k + 0.5F));
+
+            entitytntprimed.fuseTicks = world.random.nextInt(entitytntprimed.fuseTicks / 4) + entitytntprimed.fuseTicks / 8;
+            world.addEntity(entitytntprimed);
+        }
+    }
+	
+	@Override
+	public void a(World world, int i, int j, int k, Entity entity) {
+        if (entity instanceof EntityArrow && !world.isStatic) {
+            EntityArrow entityarrow = (EntityArrow) entity;
+
+            if (entityarrow.isBurning()  && !GoldenApple.getInstance().mainConfig.getBoolean("modules.antigrief.noFireArrowTnt", true)) {
+                this.postBreak(world, i, j, k, 1);
+                world.setTypeId(i, j, k, 0);
+            }
+        }
+    }
+
+}
--- src/main/java/com/bendude56/goldenapple/listener/AntigriefListener.java
+++ src/main/java/com/bendude56/goldenapple/listener/AntigriefListener.java
@@ -1,14 +1,18 @@
 package com.bendude56.goldenapple.listener;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventException;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.EventExecutor;
 import org.bukkit.plugin.RegisteredListener;
@@ -32,19 +36,50 @@ public class AntigriefListener implements Listener, EventExecutor {
 			listener = null;
 		}
 	}
+	
+	private boolean errorLoadingTntBlock = false;
 
 	private void registerEvents() {
+		errorLoadingTntBlock = false;
 		PlayerInteractEvent.getHandlerList().register(new RegisteredListener(this, this, EventPriority.NORMAL, GoldenApple.getInstance(), true));
+		BlockBurnEvent.getHandlerList().register(new RegisteredListener(this, this, EventPriority.NORMAL, GoldenApple.getInstance(), true));
+		EntityExplodeEvent.getHandlerList().register(new RegisteredListener(this, this, EventPriority.NORMAL, GoldenApple.getInstance(), true));
+		try {
+			registerTntBlock();
+		} catch (Throwable e) {
+			GoldenApple.log(Level.WARNING, "Failed to replace TNT block data. Are you running the right version of Bukkit?");
+			GoldenApple.log(Level.WARNING, (e instanceof InvocationTargetException) ? e.getCause() : e);
+			errorLoadingTntBlock = true;
+		}
 	}
 
 	private void unregisterEvents() {
 		PlayerInteractEvent.getHandlerList().unregister(this);
+		BlockBurnEvent.getHandlerList().unregister(this);
+		EntityExplodeEvent.getHandlerList().unregister(this);
+		if (!errorLoadingTntBlock) {
+			try {
+				unregisterTntBlock();
+			} catch (Throwable e) { }
+		}
+	}
+	
+	private void registerTntBlock() throws Throwable {
+		Class.forName("com.bendude56.goldenapple.antigrief.BlockTNT").getMethod("registerBlock", new Class<?>[0]).invoke(null, new Object[0]);
+	}
+	
+	private void unregisterTntBlock() throws Throwable {
+		Class.forName("com.bendude56.goldenapple.antigrief.BlockTNT").getMethod("unregisterBlock", new Class<?>[0]).invoke(null, new Object[0]);
 	}
 
 	@Override
 	public void execute(Listener listener, Event event) throws EventException {
 		if (event instanceof PlayerInteractEvent) {
 			playerInteract((PlayerInteractEvent)event);
+		} else if (event instanceof BlockBurnEvent) {
+			blockBurn((BlockBurnEvent)event);
+		} else if (event instanceof EntityExplodeEvent) {
+			entityExplode((EntityExplodeEvent)event);
 		} else {
 			GoldenApple.log(Level.WARNING, "Unrecognized event in AntigriefListener: " + event.getClass().getName());
 		}
@@ -53,7 +88,7 @@ public class AntigriefListener implements Listener, EventExecutor {
 	private void playerInteract(PlayerInteractEvent event) {
 		User u = User.getUser(event.getPlayer());
 		
-		if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.TNT && event.getPlayer().getItemInHand().getType() == Material.FLINT_AND_STEEL && GoldenApple.getInstance().mainConfig.getBoolean("modules.antigrief.noLightTnt")) {
+		if (event.getAction() == Action.RIGHT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.TNT && event.getPlayer().getItemInHand().getType() == Material.FLINT_AND_STEEL) {
 			Location l = event.getClickedBlock().getLocation();
 			if (GoldenApple.getInstance().mainConfig.getBoolean("modules.antigrief.noLightTnt", true) && (GoldenApple.getInstance().permissions == null || !u.hasPermission(AntigriefModuleLoader.tntPermission))) {
 				event.setCancelled(true);
@@ -62,6 +97,19 @@ public class AntigriefListener implements Listener, EventExecutor {
 			} else {
 				GoldenApple.log(Level.WARNING, u.getName() + " has ignited TNT at (" + l.getBlockX() + ", " + l.getBlockY() + ", " + l.getBlockZ() + ", " + l.getWorld().getName() + ")");
 			}
+		}
+	}
+	
+	private void blockBurn(BlockBurnEvent event) {
+		if (event.getBlock().getType() == Material.TNT) {
+			if (GoldenApple.getInstance().mainConfig.getBoolean("modules.antigrief.noFireTnt", true))
+				event.setCancelled(true);
+		}
+	}
+	
+	private void entityExplode(EntityExplodeEvent event) {
+		if (event.getEntityType() == EntityType.PRIMED_TNT && errorLoadingTntBlock && GoldenApple.getInstance().mainConfig.getBoolean("modules.antigrief.blockTntOnLoadFail", true)) {
+			event.setCancelled(true);
 		}
 	}
 }
