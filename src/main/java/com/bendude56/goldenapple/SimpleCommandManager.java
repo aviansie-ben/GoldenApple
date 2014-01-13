e4fc711d53aac1c5b61bc583677f728a6984f89c
bendude56 <bendude56@gmail.com>
2014-01-12 20:33:26 -0700

    Fixed issue with double registration of commands causing aliases to be removed

--- src/main/java/com/bendude56/goldenapple/SimpleCommandManager.java
+++ src/main/java/com/bendude56/goldenapple/SimpleCommandManager.java
@@ -5,30 +5,7 @@ import java.util.HashMap;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 
-import com.bendude56.goldenapple.commands.AutoLockCommand;
-import com.bendude56.goldenapple.commands.BackCommand;
-import com.bendude56.goldenapple.commands.BanCommand;
-import com.bendude56.goldenapple.commands.ChannelCommand;
-import com.bendude56.goldenapple.commands.ComplexCommand;
-import com.bendude56.goldenapple.commands.DelHomeCommand;
-import com.bendude56.goldenapple.commands.DelWarpCommand;
-import com.bendude56.goldenapple.commands.HomeCommand;
-import com.bendude56.goldenapple.commands.ImportCommand;
-import com.bendude56.goldenapple.commands.LemonPledgeCommand;
-import com.bendude56.goldenapple.commands.LockCommand;
-import com.bendude56.goldenapple.commands.MeCommand;
-import com.bendude56.goldenapple.commands.ModuleCommand;
-import com.bendude56.goldenapple.commands.MuteCommand;
-import com.bendude56.goldenapple.commands.OwnCommand;
-import com.bendude56.goldenapple.commands.PermissionsCommand;
-import com.bendude56.goldenapple.commands.SetHomeCommand;
-import com.bendude56.goldenapple.commands.SetWarpCommand;
-import com.bendude56.goldenapple.commands.SpawnCommand;
-import com.bendude56.goldenapple.commands.TpCommand;
-import com.bendude56.goldenapple.commands.TpHereCommand;
 import com.bendude56.goldenapple.commands.UnloadedCommand;
-import com.bendude56.goldenapple.commands.VerifyCommand;
-import com.bendude56.goldenapple.commands.WarpCommand;
 
 public class SimpleCommandManager extends CommandManager {
 	public static final UnloadedCommand defaultCommand = new UnloadedCommand();
@@ -36,42 +13,6 @@ public class SimpleCommandManager extends CommandManager {
 	private HashMap<String, CommandInformation> commands = new HashMap<String, CommandInformation>();
 	
 	public SimpleCommandManager() {
-		// Module - Base
-		insertCommand("gamodule", "Base", new ModuleCommand());
-		insertCommand("gaverify", "Base", new VerifyCommand());
-		insertCommand("gaimport", "Base", new ImportCommand());
-		insertCommand("gacomplex", "Base", new ComplexCommand());
-		
-		// Module - Permissions
-		insertCommand("gapermissions", "Permissions", new PermissionsCommand());
-		insertCommand("gaown", "Permissions", new OwnCommand());
-		
-		// Module - Chat
-		insertCommand("gachannel", "Chat", new ChannelCommand());
-		insertCommand("game", "Chat", new MeCommand());
-		insertCommand("galemonpledge", "Chat", new LemonPledgeCommand());
-		
-		// Module - Lock
-		insertCommand("galock", "Lock", new LockCommand());
-		insertCommand("gaautolock", "Lock", new AutoLockCommand());
-		
-		// Module - Warp
-		insertCommand("gahome", "Warp", new HomeCommand());
-		insertCommand("gasethome", "Warp", new SetHomeCommand());
-		insertCommand("gadelhome", "Warp", new DelHomeCommand());
-		insertCommand("gaspawn", "Warp", new SpawnCommand());
-		insertCommand("gaback" ,"Warp", new BackCommand());
-		insertCommand("gatp", "Warp", new TpCommand());
-		insertCommand("gatphere", "Warp", new TpHereCommand());
-		insertCommand("gawarp", "Warp", new WarpCommand());
-		insertCommand("gasetwarp", "Warp", new SetWarpCommand());
-		insertCommand("gadelwarp", "Warp", new DelWarpCommand());
-		
-		// Module - Punish
-		insertCommand("gaban", "Punish", new BanCommand());
-		insertCommand("gamute", "Punish", new MuteCommand());
-		insertCommand("gaglobalmute", "Punish", null);
-		insertCommand("gawhois", "Punish", null);
 	}
 	
 	@Override
