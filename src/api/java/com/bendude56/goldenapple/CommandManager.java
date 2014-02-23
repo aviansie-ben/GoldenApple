db99081640e9304c6501635d966375de2335f3d6
bendude56 <bendude56@gmail.com>
2014-02-23 09:36:57 -0700

    Fixed command registration for CraftBukkit 1.7.2-R0.3

--- src/api/java/com/bendude56/goldenapple/CommandManager.java
+++ src/api/java/com/bendude56/goldenapple/CommandManager.java
@@ -1,90 +1,22 @@
 package com.bendude56.goldenapple;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.command.SimpleCommandMap;
-import org.bukkit.plugin.SimplePluginManager;
 
 public abstract class CommandManager {
 	public abstract void insertCommand(String commandName, String module, CommandExecutor executor);
-	public abstract CommandInformation getCommand(String command);
+	public abstract ICommandInformation getCommand(String command);
 	public abstract CommandExecutor getDefaultCommand();
 	
-	public class CommandInformation {
-		public String name;
-		public List<String> aliases;
-		public String module;
+	public interface ICommandInformation {
+		String getName();
+		List<String> getAliases();
+		String getModule();
+		CommandExecutor getExecutor();
+		PluginCommand getCommand();
 		
-		public CommandExecutor executor;
-		public PluginCommand command;
-		
-		public CommandInformation(PluginCommand command, String module, CommandExecutor executor) {
-			this.name = command.getName();
-			this.aliases = command.getAliases();
-			this.module = module;
-			
-			this.executor = executor;
-			this.command = command;
-			
-			unregister();
-		}
-		
-		@SuppressWarnings("unchecked")
-		private void setAliases(List<String> aliases) {
-			command.setAliases(aliases);
-			try {
-				Field f = SimplePluginManager.class.getDeclaredField("commandMap");
-				f.setAccessible(true);
-				SimpleCommandMap commandMap = (SimpleCommandMap)f.get(Bukkit.getPluginManager());
-				
-				f = SimpleCommandMap.class.getDeclaredField("knownCommands");
-				f.setAccessible(true);
-				Map<String, Command> cmd = (Map<String, Command>)f.get(commandMap);
-				
-				f = SimpleCommandMap.class.getDeclaredField("aliases");
-				f.setAccessible(true);
-				Set<String> mapAliases = (Set<String>)f.get(commandMap);
-				
-				for (String alias : command.getAliases()) {
-					if (cmd.containsKey(alias)) {
-						cmd.remove(alias);
-						if (mapAliases.contains(alias))
-							mapAliases.remove(alias);
-					}
-				}
-				
-				for (String alias : aliases) {
-					if (!cmd.containsKey(alias)) {
-						cmd.put(alias, command);
-						if (!mapAliases.contains(alias))
-							mapAliases.add(alias);
-					}
-				}
-				
-				f = Command.class.getDeclaredField("activeAliases");
-				f.setAccessible(true);
-				f.set(command, aliases);
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
-		}
-		
-		public void register() {
-			setAliases(aliases);
-			command.setExecutor(executor);
-		}
-		
-		public void unregister() {
-			setAliases(new ArrayList<String>());
-			command.setExecutor(getDefaultCommand());
-		}
+		void register();
+		void unregister();
 	}
 }
--- src/main/java/com/bendude56/goldenapple/SimpleCommandManager.java
+++ src/main/java/com/bendude56/goldenapple/SimpleCommandManager.java
@@ -1,10 +1,12 @@
 package com.bendude56.goldenapple;
 
+import java.util.Collections;
 import java.util.HashMap;
-
+import java.util.List;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
-
+import org.bukkit.command.PluginCommand;
 import com.bendude56.goldenapple.commands.UnloadedCommand;
 
 public class SimpleCommandManager extends CommandManager {
@@ -21,12 +23,73 @@ public class SimpleCommandManager extends CommandManager {
 	}
 	
 	@Override
-	public CommandInformation getCommand(String command) {
+	public ICommandInformation getCommand(String command) {
 		return commands.get(command);
 	}
 
 	@Override
 	public CommandExecutor getDefaultCommand() {
 		return defaultCommand;
+	}
+	
+	public class CommandInformation implements ICommandInformation {
+		public String name;
+		public List<String> aliases;
+		public String module;
+		
+		public CommandExecutor executor;
+		public PluginCommand command;
+		
+		public HashMap<String, Command> oldCommands;
+		
+		public CommandInformation(PluginCommand command, String module, CommandExecutor executor) {
+			this.name = command.getName();
+			this.aliases = command.getAliases();
+			this.module = module;
+			
+			this.executor = executor;
+			this.command = command;
+			
+			this.oldCommands = new HashMap<String, Command>();
+			
+			unregister();
+		}
+		
+		// TODO Reimplement alias registration/unregistration
+		
+		@Override
+		public void register() {
+			command.setExecutor(executor);
+		}
+		
+		@Override
+		public void unregister() {
+			command.setExecutor(getDefaultCommand());
+		}
+
+		@Override
+		public String getName() {
+			return name;
+		}
+
+		@Override
+		public List<String> getAliases() {
+			return Collections.unmodifiableList(aliases);
+		}
+
+		@Override
+		public String getModule() {
+			return module;
+		}
+
+		@Override
+		public CommandExecutor getExecutor() {
+			return executor;
+		}
+
+		@Override
+		public PluginCommand getCommand() {
+			return command;
+		}
 	}
 }
