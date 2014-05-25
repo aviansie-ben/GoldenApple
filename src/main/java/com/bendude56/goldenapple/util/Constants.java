1b6c361e568d02d1110f24777a11deb8a1ee868f
bendude56 <bendude56@gmail.com>
2014-05-24 22:43:44 -0600

    Removed some old, outdated functions from the "util" package

--- src/main/java/com/bendude56/goldenapple/util/Constants.java
+++ src/main/java/com/bendude56/goldenapple/util/Constants.java
@@ -1,21 +1,13 @@
 package com.bendude56.goldenapple.util;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
-import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 
 public class Constants {
-	
-	private static final HashSet<Byte> transparentBlocks;
 	private static final List<EntityType> hostileMobs;
 	private static final List<EntityType> passiveMobs;
-	
-	public static HashSet<Byte> getTransparentBlocks() {
-		return transparentBlocks;
-	}
 	
 	public static List<EntityType> getHostileMobs() {
 		return hostileMobs;
@@ -26,50 +18,10 @@ public class Constants {
 	}
 	
 	static {
-		transparentBlocks = new HashSet<Byte>(32);
-		initTransparentBlocks();
 		hostileMobs = new ArrayList<EntityType>();
 		initHostileMobs();
 		passiveMobs = new ArrayList<EntityType>();
 		initPassiveMobs();
-	}
-	
-	@SuppressWarnings("deprecation")
-	private static void initTransparentBlocks() {
-		// TODO Replace this, since getId() is now deprecated
-		
-		transparentBlocks.add(((Integer) Material.AIR.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.BREWING_STAND.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.BROWN_MUSHROOM.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.CAKE.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.CROPS.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.DETECTOR_RAIL.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.DIODE_BLOCK_ON.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.DIODE_BLOCK_OFF.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.LADDER.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.LAVA.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.LEVER.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.LONG_GRASS.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.MELON_STEM.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.NETHER_STALK.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.PAINTING.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.PORTAL.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.POWERED_RAIL.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.PUMPKIN_STEM.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.RAILS.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.RED_MUSHROOM.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.RED_ROSE.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.REDSTONE_TORCH_ON.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.REDSTONE_TORCH_OFF.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.REDSTONE_WIRE.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.SAPLING.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.SIGN_POST.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.SNOW.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.TORCH.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.VINE.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.WALL_SIGN.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.WATER.getId()).byteValue());
-		transparentBlocks.add(((Integer) Material.YELLOW_FLOWER.getId()).byteValue());
 	}
 	
 	private static void initHostileMobs() {
