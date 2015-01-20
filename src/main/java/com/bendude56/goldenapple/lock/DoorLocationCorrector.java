a58d147d3458ad285f11c1496e6de50a71bb70fb
bendude56 <bendude56@gmail.com>
2015-01-19 20:36:25 -0700

    New doors and fence gates from Minecraft 1.8 can now be locked. Fixes #71.

--- src/main/java/com/bendude56/goldenapple/lock/DoorLocationCorrector.java
+++ src/main/java/com/bendude56/goldenapple/lock/DoorLocationCorrector.java
@@ -7,18 +7,34 @@ public class DoorLocationCorrector implements ILocationCorrector {
     
     @Override
     public void correctLocation(Location l) {
-        if (l.getBlock().getType() == Material.WOODEN_DOOR) {
+        Material m = l.getBlock().getType();
+        
+        if (isDoor(m)) {
             l.setY(l.getY() - 1);
-            if (l.getBlock().getType() == Material.WOODEN_DOOR) {
+            if (l.getBlock().getType() == m) {
                 return;
             }
             l.setY(l.getY() + 1);
         } else {
             l.setY(l.getY() + 1);
-            if (l.getBlock().getType() == Material.WOODEN_DOOR) {
+            if (isDoor(l.getBlock().getType())) {
                 return;
             }
             l.setY(l.getY() - 1);
+        }
+    }
+    
+    private boolean isDoor(Material m) {
+        switch (m) {
+        case ACACIA_DOOR:
+        case BIRCH_DOOR:
+        case DARK_OAK_DOOR:
+        case JUNGLE_DOOR:
+        case SPRUCE_DOOR:
+        case WOODEN_DOOR:
+            return true;
+        default:
+            return false;
         }
     }
     
